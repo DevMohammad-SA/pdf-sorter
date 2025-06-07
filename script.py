212a2bfc09556008de37d79215a740b99ef8b7c9
Mohammad Albuainain <m.albuainain@tuta.io>
2025-06-07 21:21:36 +0300

    Added check_file_existence function

--- script.py
+++ script.py
@@ -4,13 +4,24 @@ path = None
 selected_path = input(
     "Enter the full path of the folder you want to sort (Leave empty for current folder):\n"
 )
+
+
+def check_path_existence(path):
+    result = os.path.exists(path)
+    return result
+
+
 if selected_path == "":
     path = os.getcwd()
     print(f"sorting {path} ...")
 else:
-    path = selected_path
-
-print(f"sorting {path} ...")
+    while not check_path_existence(path=path):
+        selected_path = input(
+            "The directory path you have provide does not exists, please provide a full path"
+        )
+    else:
+        path = selected_path
+        print(f"sorting {path} ...")
 
 print("Creating Pictures & Videos folder...")
 try:
